3f8c16046e6544b4416677b41bc276226c9964d1
Abdullah1428 <abdullahcse1428@gmail.com>
2021-08-19 11:42:22 +0200

    deployed app to kubernetes cluster with ingress and load balancer

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -8,7 +8,7 @@ const CommentCreate = ({ postId }) => {
     e.preventDefault()
 
     try {
-      await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      await axios.post(`http://posts.com/posts/${postId}/comments`, {
         content
       })
     } catch (error) {
--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -8,7 +8,7 @@ const PostCreate = () => {
     e.preventDefault()
 
     try {
-      await axios.post('http://localhost:4000/posts', {
+      await axios.post('http://posts.com/posts/create', {
         title
       })
     } catch (error) {
